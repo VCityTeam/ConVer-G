b0238ee6d2445651780064b7dc2d84c2870376e3
Jey Puget Gil <LinkJerome@gmail.com>
2023-10-27 19:38:54 +0200

    Improvements: - Fixed database import

--- workflows/gratte_ciel/import_relational.sh
+++ workflows/gratte_ciel/import_relational.sh
@@ -125,7 +125,7 @@ curl --location 'http://localhost:8080/import/version' \
 echo "[SPARQL-to-SQL] Version GratteCiel_2009_alt_split."
 curl --location 'http://localhost:8080/import/version' \
   --header 'Content-Type: multipart/form-data' \
-  --form 'file=@"quads/named_graph/GratteCiel_2009_alt_split.ttl"'
+  --form 'file=@"triples/GratteCiel_2009_alt_split.ttl"'
 
 echo "[SPARQL-to-SQL] Versions import completed."
 
@@ -141,7 +141,7 @@ echo "[SPARQL-to-SQL] Workspace import started."
 echo "[SPARQL-to-SQL] Workspace GratteCiel_2009_2018_Workspace."
 curl --location 'http://localhost:8080/import/workspace' \
   --header 'Content-Type: multipart/form-data' \
-  --form 'file=@"quads/named_graph/GratteCiel_2009_2018_Workspace.ttl"'
+  --form 'file=@"triples/GratteCiel_2009_2018_Workspace.ttl"'
 
 echo "[SPARQL-to-SQL] Workspace import completed."
 
@@ -151,32 +151,32 @@ echo "[SPARQL-to-SQL] Version transitions import started."
 echo "[SPARQL-to-SQL] Transition Transition_2009_2009b."
 curl --location 'http://localhost:8080/import/workspace' \
   --header 'Content-Type: multipart/form-data' \
-  --form 'file=@"quads/named_graph/Transition_2009_2009b.ttl"'
+  --form 'file=@"triples/Transition_2009_2009b.ttl"'
 
 echo "[SPARQL-to-SQL] Transition Transition_2009_2012."
 curl --location 'http://localhost:8080/import/workspace' \
   --header 'Content-Type: multipart/form-data' \
-  --form 'file=@"quads/named_graph/Transition_2009_2012.ttl"'
+  --form 'file=@"triples/Transition_2009_2012.ttl"'
 
 echo "[SPARQL-to-SQL] Transition Transition_2009b_2012b."
 curl --location 'http://localhost:8080/import/workspace' \
   --header 'Content-Type: multipart/form-data' \
-  --form 'file=@"quads/named_graph/Transition_2009b_2012b.ttl"'
+  --form 'file=@"triples/Transition_2009b_2012b.ttl"'
 
 echo "[SPARQL-to-SQL] Transition Transition_2012_2015."
 curl --location 'http://localhost:8080/import/workspace' \
   --header 'Content-Type: multipart/form-data' \
-  --form 'file=@"quads/named_graph/Transition_2012_2015.ttl"'
+  --form 'file=@"triples/Transition_2012_2015.ttl"'
 
 echo "[SPARQL-to-SQL] Transition Transition_2012b_2015."
 curl --location 'http://localhost:8080/import/workspace' \
   --header 'Content-Type: multipart/form-data' \
-  --form 'file=@"quads/named_graph/Transition_2012b_2015.ttl"'
+  --form 'file=@"triples/Transition_2012b_2015.ttl"'
 
 echo "[SPARQL-to-SQL] Transition Transition_2015_2018."
 curl --location 'http://localhost:8080/import/workspace' \
   --header 'Content-Type: multipart/form-data' \
-  --form 'file=@"quads/named_graph/Transition_2015_2018.ttl"'
+  --form 'file=@"triples/Transition_2015_2018.ttl"'
 
 echo "[SPARQL-to-SQL] Version transitions import completed."
 
