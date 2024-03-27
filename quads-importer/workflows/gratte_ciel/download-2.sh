13f09eae33343b2f67dba94c04ee869596f4fdb7
Jey Puget Gil <LinkJerome@gmail.com>
2024-03-27 16:58:47 +0100

    Full BSBM integration: - BSBM is now Dockerized - Generation of the dataset

--- quads-importer/workflows/gratte_ciel/download-2.sh
+++ quads-importer/workflows/gratte_ciel/download-2.sh
@@ -4,12 +4,36 @@
 # Copy the dataset from the BSBM generated dataset
 ######################################################
 
-printf "\n%s$(date +%FT%T) - [Copy] Dataset copy started."
+printf "\n%s$(date +%FT%T) - [Copy] Dataset generation started."
 
 rm -rf ../dataset
 
-mkdir -p ../dataset/triples
+mkdir -p ../dataset/triples && cd ../dataset/triples || exit
 
-cp ../../../bsbmtools-0.2/save/* ../dataset/triples
+# Check if the argument is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <n>"
+    exit 1
+fi
 
-printf "\n%s$(date +%FT%T) - [Copy] Dataset copy completed."
+# Get the argument
+n=$1
+
+# Create the save directory if it doesn't exist
+rm -rf save
+mkdir -p save
+
+echo "------- Generating $n versions -------"
+
+# Run the command n times
+for ((i=0;i<n;i++)); do
+    echo "Generating version $i"
+    docker run -v "$PWD:/data" vcity/bsbm generate -s ttl -pc $((500 + i)) -tc $((10*i)) -ud -ppt 10
+    mv dataset.ttl "save/version-$i.split.ttl"
+    mv dataset_update.nt "save/transition-$i.nt"
+done
+
+cp save/* .
+rm -rf save
+
+printf "\n%s$(date +%FT%T) - [Copy] Dataset generation completed."
