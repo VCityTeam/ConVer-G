a08939641e18f0a245e689785f7d82119aea720f
Jey Puget Gil <LinkJerome@gmail.com>
2024-04-10 14:11:03 +0200

    Fixed Binding

--- quads-query/src/main/java/fr/cnrs/liris/jpugetgil/sparqltosql/VersioningQueryExecution.java
+++ quads-query/src/main/java/fr/cnrs/liris/jpugetgil/sparqltosql/VersioningQueryExecution.java
@@ -12,10 +12,12 @@ import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QuerySolution;
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.sparql.core.Quad;
 import org.apache.jena.sparql.core.Var;
 import org.apache.jena.sparql.engine.ResultSetStream;
 import org.apache.jena.sparql.engine.binding.Binding;
+import org.apache.jena.sparql.engine.binding.BindingBuilder;
 import org.apache.jena.sparql.util.Context;
 import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
@@ -98,9 +100,11 @@ public class VersioningQueryExecution implements QueryExecution {
                     }
                 }
 
+                BindingBuilder bindingBuilder = Binding.builder();
                 for (String var : variables) {
                     Var variable = Var.alloc(var);
                     Node variableValue;
+
                     if (rs.getString("name$" + var) != null) {
                         String value = rs.getString("name$" + var);
                         String valueType = rs.getString("type$" + var);
@@ -113,9 +117,11 @@ public class VersioningQueryExecution implements QueryExecution {
                     if (!vars.contains(variable)) {
                         vars.add(variable);
                     }
-                    Binding binding = Binding.builder().add(variable, variableValue).build();
-                    bindings.add(binding);
+
+                    bindingBuilder.add(variable, variableValue);
                 }
+
+                bindings.add(bindingBuilder.build());
             }
 
             return ResultSetStream.create(vars, bindings.iterator());
