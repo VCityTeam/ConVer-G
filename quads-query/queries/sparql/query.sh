56ae23d80d185723e078312da8c8a8e1730966f5
Jey Puget Gil <LinkJerome@gmail.com>
2024-04-10 11:31:12 +0200

    Query script - Created a query script

---
+++ quads-query/queries/sparql/query.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+######################################################
+# Find all the queries and send them to the SPARQL-to-SQL and Blazegraph servers
+######################################################
+
+printf "\n%s$(date +%FT%T) - [Query - SPARQL-to-SQL] Query started."
+
+find . -type f -name "sparql*" -print0 | while IFS= read -r -d '' file
+do
+    printf "\n%s$(date +%FT%T) - [Query - SPARQL-to-SQL] Query $file"
+    name=$(basename "$file")
+
+    curl --location http://localhost:8081/rdf/sparql -X POST --data-binary @"$file" \
+      --header 'Content-Type: application/sparql-query' \
+      --output "sts-$name.json" \
+      --header 'Accept: application/sparql-results+json'
+done
+
+printf "\n%s$(date +%FT%T) - [Query - SPARQL-to-SQL] Query completed."
+
+
+printf "\n%s$(date +%FT%T) - [Query - Blazegraph] Query started."
+
+find . -type f -name "sparql*" -print0 | while IFS= read -r -d '' file
+do
+    printf "\n%s$(date +%FT%T) - [Query - Blazegraph] Query $file"
+    name=$(basename "$file")
+
+    curl --location http://localhost:9999/blazegraph/namespace/kb/sparql -X POST --data-binary @"$file" \
+      --header 'Content-Type: application/sparql-query' \
+      --output "blazegraph-$name.json" \
+      --header 'Accept: application/sparql-results+json'
+done
+
+printf "\n%s$(date +%FT%T) - [Query - Blazegraph] Query completed."
+
